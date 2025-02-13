309168a377eaa466d614e6660b740fda91e8a789
Jinyoung Yoon <jyoo7375@gmail.com>
2025-02-12 21:47:48 -0500

    Complete objective2lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,9 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Jay Yoon";
+		int age = 28;
+		String state = "New York";
+
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
