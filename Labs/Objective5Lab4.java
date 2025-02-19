287d76761ff43b61881e92bdd4ab3d9fdda1dc3d
Jinyoung Yoon <jyoo7375@gmail.com>
2025-02-18 20:13:59 -0500

    Complete Objective5lab4

---
+++ Labs/Objective5Lab4.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Objective5Lab4 {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		int num;
+
+		System.out.println("Please enter a number: ");
+		num = input.nextInt();
+
+		if(num % 2 == 0){
+			System.out.println("The number is even");
+		}
+		else {
+			System.out.println("The number is odd");
+		}
+
+		input.close();
+	}
+}
