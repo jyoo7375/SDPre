e4f2a2ae7274a858a1db2bb747850d25eb2750e6
Jinyoung Yoon <jyoo7375@gmail.com>
2025-02-21 01:42:55 -0500

    Complete Objective7lab4

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4 {
+	public static void main (String args[]){
+		int i= 1;
+		int sum = 0;
+
+		while(i <= 20){
+			sum = sum + i;
+			i++;
+		}
+		System.out.println(sum);
+	}
+}
